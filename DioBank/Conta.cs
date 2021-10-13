d0d24ad790ac70f4df106af9fe9c20c327eb1899
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-10-12 22:59:54 -0300

    Projeto Original realizado em aula

---
+++ DioBank/Conta.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioBank
+{
+    class Conta
+    {
+        public TipoConta TipoConta { get; private set; }
+
+        public string Nome{ get; private set; }
+        public double Saldo { get; private set; }
+        public double Credito{ get; private set; }
+
+        public Conta(TipoConta tipoConta, string nome, double saldo, double credito)
+        {
+            this.TipoConta = tipoConta;
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Credito = credito;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (Saldo + Credito < valorSaque)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é R${1}.", Nome, Saldo);
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é de R${1}.", Nome, Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTransferencia))
+                contaDestino.Depositar(valorTransferencia);
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
+            
+        }
+
+    }
+}
---
+++ DioBank/TipoConta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioBank
+{
+    enum TipoConta
+    {
+        PessoaFísica = 1,
+        PessoaJurídica = 2
+    }
+}
